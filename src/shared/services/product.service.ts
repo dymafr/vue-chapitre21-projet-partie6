9b7728029deb3243135595d142aeee8392898866
dymafr <contact@dyma.fr>
2022-08-29 09:59:11 +0200

    fix: new urlparams

--- src/shared/services/product.service.ts
+++ src/shared/services/product.service.ts
@@ -2,24 +2,26 @@ import type {
   FiltersInterface,
   ProductFormInterface,
   ProductInterface,
-} from "@/shared/interfaces";
+} from '@/shared/interfaces';
 
-const BASE_URL = "https://restapi.fr/api/vueprojectproducts";
+const BASE_URL = 'https://restapi.fr/api/vueprojectproducts';
 
 export async function fetchProductsWithFilter(
   filter: FiltersInterface,
   page: number
 ): Promise<ProductInterface[] | ProductInterface> {
   const query = new URLSearchParams();
-  if (filter.category !== "all") {
-    query.append("category", filter.category);
+  if (filter.category !== 'all') {
+    query.append('category', filter.category);
   }
-  query.append("limit", "20");
+  query.append('limit', '20');
   if (page !== 1) {
-    query.append("skip", (page - 1) * 20 + "");
+    query.append('skip', (page - 1) * 20 + '');
   }
-  query.append("price", `$lte:${filter.priceRange[1]}`);
-  query.append("price", `$gte:${filter.priceRange[0]}`);
+  query.append(
+    'price',
+    `{"$gte":${filter.priceRange[0]}, "$lte":${filter.priceRange[1]}}`
+  );
   const products = await (await fetch(`${BASE_URL}?${query}`)).json();
   return products;
 }
@@ -30,7 +32,7 @@ export async function fetchProducts(): Promise<ProductInterface[]> {
 
 export async function deleteProduct(productId: string): Promise<string> {
   await fetch(`${BASE_URL}/${productId}`, {
-    method: "DELETE",
+    method: 'DELETE',
   });
   return productId;
 }
@@ -39,11 +41,11 @@ export async function addProduct(
   product: ProductFormInterface
 ): Promise<ProductInterface> {
   const newProduct = await (
-    await fetch("https://restapi.fr/api/projetproducts", {
-      method: "POST",
+    await fetch('https://restapi.fr/api/projetproducts', {
+      method: 'POST',
       body: JSON.stringify(product),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
   ).json();
@@ -61,10 +63,10 @@ export async function editProduct(
 ): Promise<ProductInterface> {
   const updatedProduct = await (
     await fetch(`${BASE_URL}/${productId}`, {
-      method: "PATCH",
+      method: 'PATCH',
       body: JSON.stringify(product),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
   ).json();
